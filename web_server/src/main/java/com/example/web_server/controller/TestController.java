d41fe27a75d65bcd08572c3b1ec061a69f885d8c
codersue <devsue@foxmail.com>
2023-02-22 15:55:41 +0800

    测试接口

---
+++ web_server/src/main/java/com/example/web_server/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.web_server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "test")
+public class TestController {
+
+    @GetMapping(value = "hello")
+    public String test(){
+        return "hello world";
+    }
+}
